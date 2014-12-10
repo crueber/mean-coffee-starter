178e388e34e8e3f5cf8a58f8eb09a331d286143d
Christopher WJ Rueber <crueber@gmail.com>
2014-12-10 12:10:19 -0600

    Pulling grunt-concurrent for now.

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,6 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-nodemon');
-  grunt.loadNpmTasks('grunt-concurrent');
 
-  grunt.registerTask('default', ['concurrent']);
+  grunt.registerTask('default', ['nodemon']);
 };
